b8d7e3d071ed2e1a6383dc11d2b4547592745bda
rianders <rianders@docs.rutgers.edu>
2018-04-02 16:07:31 -0400

    added the TopBar.vue component

---
+++ voxboxclient/src/components/TopBar.vue
@@ -0,0 +1,24 @@
+<script>
+export default {
+  name: "topbar",
+  props: {
+    msg: String
+  }
+};
+</script>
+<template>
+    <div id="topBar">
+        <div class="topLeft">
+            <p>Favorite
+                <br>&#9733;</p>
+        </div>
+        <div class="sentence" id="sentence">
+            <p id="saything">Example Text</p>
+        </div>
+        <div id="saybutton" class="topRight">
+            <img src="https://i.imgur.com/UCxmzJx.png" alt="logo" /> </div>
+        </div>
+    </div>
+</template>
+
+
