38f48e7f5f1304bd05c0f9c4d8485016b2fb280a
rianders <rianders@docs.rutgers.edu>
2018-04-02 16:47:41 -0400

    added the majority of componets. TODO: WordLibrary component

--- voxboxclient/src/App.vue
+++ voxboxclient/src/App.vue
@@ -1,297 +1,34 @@
+<script>
+  import SpeechBar from "./components/SpeechBar.vue"
+  import CommonWords from "./components/CommonWords.vue"
+  import WordGrid from "./components/WordGrid.vue"
+
+
+  export default {
+  name: 'app',
+  components: {
+    SpeechBar, 
+    CommonWords,
+    WordGrid
+  }
+}
+</script>
+
 <template>
   <div id="app">
-
        <!-- <router-view/>-->
-<div class = "topBar">
-  <div class = "topLeft"><p>Favorite<br>&#9733;</p></div><div class = "sentence">
-    </div><div class = "topRight">
-  </div>
-  
-  
-</div>
-
+ <SpeechBar></SpeechBar>
+ <!-- <WordLibrary> -->
+  <div class = "bottomChunk" id="button">
+   <CommonWords></CommonWords>
+   <WordGrid></WordGrid>        
+ </div>
  
- <div class = "bottomChunk" id="button">
-  <div class = "keywords">
-   <div class = "key say"> 
-      <h1 class="Words1">Yes</h1>
-       <img src="https://i.imgur.com/e8u8t8v.png">
-   </div>
-   <div class = "key say">
-      <h1 class="Words1">No</h1>
-      <img src="https://i.imgur.com/AoNCI7r.png">
-   </div>
-   <div class = "key say">
-    <h1 class="Words1">I</h1>
-     <img src = "https://i.imgur.com/Yh7PKdb.png">
-   </div>
-   <div class = "key say">
-      <h1 class="Words1">He</h1>
-     <img src = "https://i.imgur.com/4bu7gmK.png">
-   </div>
-   <div class = "key say">
-      <h1 class="Words1">She</h1>
-     <img src = "https://i.imgur.com/klwq1tQ.png"> 
-   </div>
-   <div class = "key say">
-      <h1 class="Words1">It</h1>
-      <img src="https://i.imgur.com/4jwKpOp.png">
-   </div>   
- </div>
- </div>
-
-
- <div class="menu">
-   <div class="item2 say">
-     <p class="Words3">Hi</p>
-     <img src="https://i.imgur.com/UCxmzJx.png">
-   </div>
-     <div class="item2 say">
-     <p class="Words3">My Name</p>
-     <img src="https://i.imgur.com/UCxmzJx.png">
-   </div>
-     <div class="item2 say">
-     <p class="Words3">Is Voxbox</p>
-     <img src="https://i.imgur.com/UCxmzJx.png">
-   </div>
-   </div>
-
-
-
-<!-- Folder1/Things -->
-  <div class= "myBtn_multi">
-     <img src = "https://i.imgur.com/kkqg1WH.png">
-   </div>
-
- <!-- The Modal -->
-<div class="modal modal_multi">
-  <!-- Modal content -->
-  <div class="modal-content">
-    <span class="close close_multi"> &times; </span>
-       <div class="item say">
-     <h1 class="Words1">Toys</h1>
-     <img src = "https://i.imgur.com/4jwKpOp.png">
-   </div>
-    <div class="item say">
-     <h1 class="Words1">Box</h1>
-     <img src = "https://i.imgur.com/4jwKpOp.png">
-   </div>
-    <div class="item say">
-     <h1 class="Words1">Food</h1>
-     <img src = "https://i.imgur.com/4jwKpOp.png">
-   </div>
-  </div>
-</div> 
-
-<!-- Folder2/Action -->
- <div class= "myBtn_multi">
-     <img src = "https://i.imgur.com/pBsvtcK.png">
-   </div>
-<!-- The Modal -->
-<div class= "modal modal_multi" >
-  <!-- Modal content -->
-  <div class="modal-content">
-    <span class="close close_multi">&times;</span>
-       <div class="item say">
-     <h1 class="Words1">Say</h1>
-     <img src = "https://i.imgur.com/4jwKpOp.png">
-   </div>
-    <div class="item say">
-     <h1 class="Words1">Sing</h1>
-     <img src = "https://i.imgur.com/4jwKpOp.png">
-   </div>
-    <div class="item say">
-     <h1 class="Words1">Stop</h1>
-     <img src = "https://i.imgur.com/4jwKpOp.png">
-   </div>
-  </div>
-</div>
-
-<!-- Folder3/Clothes -->
- <div class= "myBtn_multi">
-     <img src = "https://i.imgur.com/oeQe0Xk.png">
-   </div>
-<!-- The Modal -->
-<div class= "modal modal_multi" >
-  <!-- Modal content -->
-  <div class="modal-content">
-    <span class="close close_multi">&times;</span>
-       <div class="item say">
-     <h1 class="Words1">Shirt</h1>
-     <img src = "https://i.imgur.com/ID5ieEG.png">
-   </div>
-    <div class="item say">
-     <h1 class="Words1">Pants</h1>
-     <img src = "https://i.imgur.com/9pWQJSd.png">
-   </div>
-    <div class="item say">
-     <h1 class="Words1">Jacket</h1>
-     <img src = "https://i.imgur.com/5Y6UkLd.png">
-   </div>
-  </div>
-</div>
-
-<!-- Folder4/Travel -->
- <div class= "myBtn_multi">
-     <img src = "https://i.imgur.com/cTMnYlV.png">
-   </div>
-<!-- The Modal -->
-<div class= "modal modal_multi" >
-  <!-- Modal content -->
-  <div class="modal-content">
-    <span class="close close_multi">&times;</span>
-   <div class="item say">
-     <h1 class="Words1">America</h1>
-     <img src = "https://i.imgur.com/4jwKpOp.png">
-   </div>
-    <div class="item say">
-     <h1 class="Words1">Europe</h1>
-     <img src = "https://i.imgur.com/4jwKpOp.png">
-   </div>
-    <div class="item say">
-     <h1 class="Words1">Asia</h1>
-     <img src = "https://i.imgur.com/4jwKpOp.png">
-   </div>
-  </div>
-</div>
-
-<!-- Folder5/Places -->
- <div class= "myBtn_multi">
-     <img src = "https://i.imgur.com/dMMkTt7.png">
-   </div>
-<!-- The Modal -->
-<div class= "modal modal_multi" >
-  <!-- Modal content -->
-  <div class="modal-content">
-    <span class="close close_multi"> &times;</span>
-       <div class="item say">
-     <h1 class="Words1">Home</h1>
-     <img src = "https://i.imgur.com/4jwKpOp.png">
-   </div>
-    <div class="item say">
-     <h1 class="Words1">School</h1>
-     <img src = "https://i.imgur.com/4jwKpOp.png">
-   </div>
-    <div class="item say">
-     <h1 class="Words1">Hospital</h1>
-     <img src = "https://i.imgur.com/4jwKpOp.png">
-   </div>
-  </div>
-</div> 
-
-<!-- Folder6/People -->
- <div class= "myBtn_multi">
-     <img src = "https://i.imgur.com/Xy1Wvlz.png">
-   </div>
-<!-- The Modal -->
-<div class= "modal modal_multi" >
-  <!-- Modal content -->
-  <div class="modal-content">
-    <span class="close close_multi">&times;</span>
-       <div class="item say">
-     <h1 class="Words1">Mom</h1>
-     <img src = "https://i.imgur.com/4jwKpOp.png">
-   </div>
-    <div class="item say">
-     <h1 class="Words1">Dad</h1>
-     <img src = "https://i.imgur.com/4jwKpOp.png">
-   </div>
-    <div class="item say">
-     <h1 class="Words1">Teacher</h1>
-     <img src = "https://i.imgur.com/4jwKpOp.png">
-   </div>
-  </div>
-</div>
-
-<!-- Folder7/Colors -->
- <div class= "myBtn_multi">
-     <img src = "https://i.imgur.com/w8lrWUO.png">
-   </div>
-<!-- The Modal -->
-<div class= "modal modal_multi" >
-  <!-- Modal content -->
-  <div class="modal-content">
-    <span class="close close_multi">&times;</span>
-       <div class="item say">
-     <h1 class="Words1">Blue</h1>
-     <img src = "https://i.imgur.com/7T9eCS6.png">
-   </div>
-    <div class="item say">
-     <h1 class="Words1">Red</h1>
-     <img src = "https://i.imgur.com/erGwWDg.png">
-   </div>
-    <div class="item say">
-     <h1 class="Words1">Yellow</h1>
-     <img src = "https://i.imgur.com/9pWQJSd.png">
-   </div>
-  </div>
-</div>
-
-<!-- Folder8/Emergency -->
- <div class= "myBtn_multi">
-     <img src = "https://i.imgur.com/BU9oclh.png">
-   </div>
-<!-- The Modal -->
-<div class= "modal modal_multi" >
-  <!-- Modal content -->
-  <div class="modal-content">
-    <span class="close close_multi">&times;</span>
-         <div class="item say">
-     <h1 class="Words1">Help</h1>
-     <img src = "https://i.imgur.com/4jwKpOp.png">
-   </div>
-    <div class="item say">
-     <h1 class="Words1">Call 911</h1>
-     <img src = "https://i.imgur.com/4jwKpOp.png">
-   </div>
-    <div class="item say">
-     <h1 class="Words1">Stop</h1>
-     <img src = "https://i.imgur.com/4jwKpOp.png">
-   </div>
-  </div>
-</div>
-
-<div class="item say">
-    <h1 class="Words1">Who</h1>
-     <img src = "https://i.imgur.com/4jwKpOp.png">
-   </div>
-   <div class="item say">
-     <h1 class="Words1">What</h1>
-     <img src = "https://i.imgur.com/4jwKpOp.png">
-   </div>
-   <div class="item say">
-     <h1 class="Words1">Why</h1>
-     <img src = "https://i.imgur.com/4jwKpOp.png">
-   </div>
-   <div class="item say">
-     <h1 class="Words1">When</h1>
-     <img src = "https://i.imgur.com/4jwKpOp.pngp">
-   </div>
-   <div class="item say">
-     <h1 class="Words1">How</h1>
-     <img src = "https://i.imgur.com/4jwKpOp.png">
-   </div>
-   <div class="item say">
-     <h1 class="Words1">Where</h1>
-     <img src = "https://i.imgur.com/4jwKpOp.png">
-   </div>
-   <div class="item say say">
-     <h1 class="Words1">Want</h1>
-     <img src = "https://i.imgur.com/4jwKpOp.png">
-   </div>
-   <div class="item say">
-     <h1 class="Words1">Time</h1>
-     <img src = "https://i.imgur.com/4jwKpOp.png">
-  </div>
-    <div class="item say">
-     <h1 class="Words1">Love</h1>
-     <img src = "https://i.imgur.com/nxI4WbJ.png">
-   </div>
-
-
+ <!-- </WordLibrary> -->
 
  </div>  <!-- app div -->
 </template>
+
 
 <style>
 #app {
---
+++ voxboxclient/src/components/CommonWords.vue
@@ -0,0 +1,38 @@
+<script>
+export default {
+  name: "CommonWords",
+  props: {
+    msg: String
+  }
+};
+</script>
+<template>
+    <div id="CommonWords">
+        <div class="keywords">
+            <div class="key say">
+                <h1 class="Words1">Yes</h1>
+                <img src="https://i.imgur.com/e8u8t8v.png">
+            </div>
+            <div class="key say">
+                <h1 class="Words1">No</h1>
+                <img src="https://i.imgur.com/AoNCI7r.png">
+            </div>
+            <div class="key say">
+                <h1 class="Words1">I</h1>
+                <img src="https://i.imgur.com/Yh7PKdb.png">
+            </div>
+            <div class="key say">
+                <h1 class="Words1">He</h1>
+                <img src="https://i.imgur.com/4bu7gmK.png">
+            </div>
+            <div class="key say">
+                <h1 class="Words1">She</h1>
+                <img src="https://i.imgur.com/klwq1tQ.png">
+            </div>
+            <div class="key say">
+                <h1 class="Words1">It</h1>
+                <img src="https://i.imgur.com/4jwKpOp.png">
+            </div>
+        </div>
+    </div>
+</template>
