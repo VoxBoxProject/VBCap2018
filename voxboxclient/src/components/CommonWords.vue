adf3ce109f6adabbed4d8a90a3a4160dd43aa046
LaurenRichardson <lnr38@scarletmail.rutgers.edu>
2018-04-11 15:28:40 -0400

    Updated the div name to "Item"

--- voxboxclient/src/components/CommonWords.vue
+++ voxboxclient/src/components/CommonWords.vue
@@ -9,27 +9,27 @@ export default {
 <template>
     <div id="CommonWords">
         <div class="keywords">
-            <div class="key say">
+            <div class="Item">
                 <h1 class="Words1">Yes</h1>
                 <img src="../assets/Yes.png" alt = "Yes">
             </div>
-            <div class="key say">
+            <div class="Item">
                 <h1 class="Words1">No</h1>
                 <img src="../assets/No.png" alt = "No">
             </div>
-            <div class="key say">
+            <div class="Item">
                 <h1 class="Words1">I</h1>
                 <img src="../assets/I.png" alt = "I">
             </div>
-            <div class="key say">
+            <div class="Item">
                 <h1 class="Words1">He</h1>
                 <img src="../assets/He.png" alt = "He">
             </div>
-            <div class="key say">
+            <div class="Item">
                 <h1 class="Words1">She</h1>
                 <img src="../assets/She.png" alt = "She">
             </div>
-            <div class="key say">
+            <div class="Item">
                 <h1 class="Words1">It</h1>
                 <img src="../assets/0058-500x500.png" alt = "It">
             </div>
