dd1bacd3e9bde1515fef5ff5dad27c9271c6b9c1
Caroline <cwr31@rutgers.edu>
2018-04-11 16:58:05 -0400

    replaced Words1 to Words

--- voxboxclient/src/components/CommonWords.vue
+++ voxboxclient/src/components/CommonWords.vue
@@ -10,27 +10,27 @@ export default {
     <div id="CommonWords">
         <div class="CommonWords">
             <div class="Item">
-                <h1 class="Words1">Yes</h1>
+                <h1 class="Words">Yes</h1>
                 <img src="../assets/Yes.png" alt = "Yes">
             </div>
             <div class="Item">
-                <h1 class="Words1">No</h1>
+                <h1 class="Words">No</h1>
                 <img src="../assets/No.png" alt = "No">
             </div>
             <div class="Item">
-                <h1 class="Words1">I</h1>
+                <h1 class="Words">I</h1>
                 <img src="../assets/I.png" alt = "I">
             </div>
             <div class="Item">
-                <h1 class="Words1">He</h1>
+                <h1 class="Words">He</h1>
                 <img src="../assets/He.png" alt = "He">
             </div>
             <div class="Item">
-                <h1 class="Words1">She</h1>
+                <h1 class="Words">She</h1>
                 <img src="../assets/She.png" alt = "She">
             </div>
             <div class="Item">
-                <h1 class="Words1">It</h1>
+                <h1 class="Words">It</h1>
                 <img src="../assets/0058-500x500.png" alt = "It">
             </div>
         </div>
