e61bfc116912bfcabd2502fe93aa643cc666f07f
cr01 <cwr5@outlook.com>
2018-04-20 21:21:33 -0400

    added in script for modals

--- voxboxclient/src/components/WordGrid.vue
+++ voxboxclient/src/components/WordGrid.vue
@@ -3,6 +3,49 @@ export default {
   name: "WordGrid",
   props : {
     msg: String
+  }, 
+  methods:{
+  // Get the modal
+
+  var modalparent = document.getElementsByClassName("modal_multi");
+
+  // Get the button that opens the modal
+
+  var modal_btn_multi = document.getElementsByClassName("myBtn_multi");
+
+  // Get the <span> element that closes the modal
+  var span_close_multi = document.getElementsByClassName("close_multi");
+
+  // When the user clicks the button, open the modal
+  setDataIndex() {
+    for (let i = 0; i < modal_btn_multi.length; i++) {
+      modal_btn_multi[i].setAttribute("data-index", i);
+      modalparent[i].setAttribute("data-index", i);
+      span_close_multi[i].setAttribute("data-index", i);
+    }
+  }
+
+  for (let i = 0; i < modal_btn_multi.length; i++) {
+    modal_btn_multi[i].onclick() {
+      var ElementIndex = this.getAttribute("data-index");
+      modalparent[ElementIndex].style.display = "block";
+    };
+
+    // When the user clicks on <span> (x), close the modal
+    span_close_multi[i].onclick() {
+      var ElementIndex = this.getAttribute("data-index");
+      modalparent[ElementIndex].style.display = "none";
+    };
+  }
+
+  window.onload() {
+    setDataIndex();
+  };
+
+  window.onclick(event) {
+    if (event.target === modalparent[event.target.getAttribute("data-index")]) {
+      modalparent[event.target.getAttribute("data-index")].style.display = "none";
+    }
   }
 };
 </script>
