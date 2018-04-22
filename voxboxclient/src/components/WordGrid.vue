bd68ed4f48fa36a6f34f3011e3969e1c32ef82f3
cr01 <cwr5@outlook.com>
2018-04-22 19:37:17 -0400

    Update WordGrid.vue
    i tried using https://alligator.io/vuejs/vue-modal-component/

--- voxboxclient/src/components/WordGrid.vue
+++ voxboxclient/src/components/WordGrid.vue
@@ -1,41 +1,26 @@
 <script>
+import modal from "modal.vue"
 export default {
-  name: "WordGrid",
-  props : {
-    msg: String
+    name: "WordGrid",
+    props : {
+        msg: String
+    },
+    components: {
+      modal,
+    },    
+  data () {
+    return {
+    isModalVisible: false,
+    };
+  },
+  methods: {
+    showModal() {
+      this.isModalVisible = true;
+    },
+    closeModal() {
+      this.isModalVisible = false;
+    }
   }
-  /*
-  ,
-  methods:{
-    //https://www.w3schools.com/howto/howto_css_modals.asp
-      // Get the modal
-      
-        modal = document.getElementById('modal_multi'),
-
-        // Get the button that opens the modal
-        btn = document.getElementById("myBtn_multi"),
-
-        // Get the <span> element that closes the modal
-        span = document.getElementsByClassName("close_multi"),
-
-        // When the user clicks on the button, open the modal 
-        btn.onclick: function() {
-            modal.style.display = "block";
-        },
-
-        // When the user clicks on <span> (x), close the modal
-        span.onclick: function() {
-            modal.style.display = "none";
-        },
-
-        // When the user clicks anywhere outside of the modal, close it
-        window.onclick: function(event) {
-            if (event.target == modal) {
-                modal.style.display = "none";
-            }
-        }
-        
-  }*/
 };
 </script>
 <template>
@@ -51,36 +36,20 @@ export default {
        <img v-bind:src="'/img/' + word.src"  v-bind:alt="word.value" />
      
      </div> <!--unorganizeed word-->
-<!--
-    <!- -Folders has folder which has words- ->
+
+
     <div v-for="(folder) in $store.state.Folders" 
         class = "myBtn_multi">
-        <!- -should select the element that isn't the 3rd nested array
-        ... shouldn't be creating 2 item classes within myBtn, the 2nd one looks like its trying to access array- ->
-        <div v-for="(words) in folder">
-            <div class="item say"> <!- -v-on:click="showModal = true"- ->
+        <div v-for="(words) in folder
+              @click="showModal">
+            <div class="item say">
                 <h1 class="words"> {{ words[0].value }} </h1>
                 <img v-bind:src="'/img/' + words[0].src"  v-bind:alt="words[0].value" />
             </div>
-        </div> <!- -words- ->
-    </div> <!- -folders- ->
-
-    
-        <!- -Modal- ->
-    <div class="modal modal_multi">
-        <!- - Modal content - ->
-        <div class="modal-content">
-            <span class="close close_multi"> &times; </span>
-            
-            <!- -this isn't appearing
-            maybe refer the arrays in folders by index- ->
-            <div v-for="(word) in $store.state.Folders.folder.words" class="item say">
-                <h1 class="words">{{ word.value }}</h1>
-                <img v-bind:src="'/img/' + word.src"  v-bind:alt="word.value" />
-            </div>
         </div>
-    </div> <!- -modal modal_multi - ->
-   -->
-  </div> <!-- Vocab -->
-</div> <!-- WordGrid -->
+    </div>
+
+    <modal v-show="isModalVisible" @close="closeModal"/>
+  </div>
+</div>
 </template>
