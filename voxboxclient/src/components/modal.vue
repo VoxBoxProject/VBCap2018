ae1770152d7b4cb1f9a7765fd295d43d287210f4
cr01 <cwr5@outlook.com>
2018-04-22 19:37:49 -0400

    Create modal.vue
    still editing the class names

---
+++ voxboxclient/src/components/modal.vue
@@ -0,0 +1,71 @@
+<script>
+  export default {
+    name: 'modal',
+    methods: {
+      close() {
+        this.$emit('close');
+      },
+    },
+  };
+</script>
+<template>
+  <transition name="modal-fade">
+    <div class="modal">
+      <div class="modal-content"
+        role="dialog"
+      >
+        <header
+          class="modal-header"
+          id="modalTitle"
+        >
+          <slot name="header">
+            This is the default tile!
+
+            <button
+              type="button"
+              class="btn-close"
+              @click="close"
+              aria-label="Close modal"
+            >
+              x
+            </button>
+          </slot>
+        </header>
+        <section
+          class="modal-body"
+          id="modalDescription"
+        >
+          <slot name="body">
+            I'm the default body!
+          </slot>
+        </section>
+        <footer class="modal-footer">
+          <slot name="footer">
+            I'm the default footer!
+
+            <button
+              type="button"
+              class="btn-green"
+              @click="close"
+              aria-label="Close modal"
+            >
+              Close me!
+            </button>
+          </slot>
+        </footer>
+      </div>
+    </div>
+  </transition>
+</template>
+
+<style>
+ .modal-fade-enter,
+  .modal-fade-leave-active {
+    opacity: 0;
+  }
+
+  .modal-fade-enter-active,
+  .modal-fade-leave-active {
+    transition: opacity .5s ease
+  }
+</style>
