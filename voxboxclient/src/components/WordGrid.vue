c1f783ff14c867bd023351e80cf8496036684096
cr01 <cwr5@outlook.com>
2018-04-22 15:12:41 -0400

    adjusting v-for placement of folder and modals

--- voxboxclient/src/components/WordGrid.vue
+++ voxboxclient/src/components/WordGrid.vue
@@ -3,50 +3,39 @@ export default {
   name: "WordGrid",
   props : {
     msg: String
-  }, 
+  }
+  /*
+  ,
   methods:{
-  // Get the modal
+    //https://www.w3schools.com/howto/howto_css_modals.asp
+      // Get the modal
+      
+        modal = document.getElementById('modal_multi'),
 
-  var modalparent = document.getElementsByClassName("modal_multi");
+        // Get the button that opens the modal
+        btn = document.getElementById("myBtn_multi"),
 
-  // Get the button that opens the modal
+        // Get the <span> element that closes the modal
+        span = document.getElementsByClassName("close_multi"),
 
-  var modal_btn_multi = document.getElementsByClassName("myBtn_multi");
+        // When the user clicks on the button, open the modal 
+        btn.onclick: function() {
+            modal.style.display = "block";
+        },
 
-  // Get the <span> element that closes the modal
-  var span_close_multi = document.getElementsByClassName("close_multi");
+        // When the user clicks on <span> (x), close the modal
+        span.onclick: function() {
+            modal.style.display = "none";
+        },
 
-  // When the user clicks the button, open the modal
-  setDataIndex() {
-    for (let i = 0; i < modal_btn_multi.length; i++) {
-      modal_btn_multi[i].setAttribute("data-index", i);
-      modalparent[i].setAttribute("data-index", i);
-      span_close_multi[i].setAttribute("data-index", i);
-    }
-  }
-
-  for (let i = 0; i < modal_btn_multi.length; i++) {
-    modal_btn_multi[i].onclick() {
-      var ElementIndex = this.getAttribute("data-index");
-      modalparent[ElementIndex].style.display = "block";
-    };
-
-    // When the user clicks on <span> (x), close the modal
-    span_close_multi[i].onclick() {
-      var ElementIndex = this.getAttribute("data-index");
-      modalparent[ElementIndex].style.display = "none";
-    };
-  }
-
-  window.onload() {
-    setDataIndex();
-  };
-
-  window.onclick(event) {
-    if (event.target === modalparent[event.target.getAttribute("data-index")]) {
-      modalparent[event.target.getAttribute("data-index")].style.display = "none";
-    }
-  }
+        // When the user clicks anywhere outside of the modal, close it
+        window.onclick: function(event) {
+            if (event.target == modal) {
+                modal.style.display = "none";
+            }
+        }
+        
+  }*/
 };
 </script>
 <template>
@@ -64,31 +53,32 @@ export default {
      </div> <!--unorganizeed word-->
 
     <!--Folders has folder which has words-->
-    <div v-for="(folder) in $store.state.Folders" class = "myBtn_multi" >
-        <!--should select the element that isn't the 3rd nested array-->
-        <div v-for="(words) in folder" >
-            <pre> {{ words }}</pre>
-
-            <div class="item say">
-                <h1 class="words"> {{ words.value }} </h1>
-                <img v-bind:src="'/img/' + words.src"  v-bind:alt="words.value" />
+    <div v-for="(folder) in $store.state.Folders" 
+        class = "myBtn_multi">
+        <!--should select the element that isn't the 3rd nested array
+        ... shouldn't be creating 2 item classes within myBtn, the 2nd one looks like its trying to access array-->
+        <div v-for="(words) in folder">
+            <div class="item say"> <!--v-on:click="showModal = true"-->
+                <h1 class="words"> {{ words[0].value }} </h1>
+                <img v-bind:src="'/img/' + words[0].src"  v-bind:alt="words[0].value" />
             </div>
-
-        <!--Modal-->
-        <div class="modal modal_multi">
-            <!-- Modal content -->
-            <div class="modal-content">
-             <span class="close close_multi"> &times; </span>
-             
-             <!--this isn't appearing-->
-                <div v-for="(word) in words" class="item say">
-                    <h1 class="words">{{ word.value }}</h1>
-                    <img v-bind:src="'/img/' + word.src"  v-bind:alt="word.value" />
-                </div>
-            </div>
-        </div> <!--modal modal_multi -->
         </div> <!--words-->
     </div> <!--folders-->
+
+        <!--Modal-->
+    <div class="modal modal_multi">
+        <!-- Modal content -->
+        <div class="modal-content">
+            <span class="close close_multi"> &times; </span>
+            
+            <!--this isn't appearing-->
+            <div v-for="(word) in $store.state.Folders.folder.words" class="item say"> <!--may refer by index-->
+                <h1 class="words">{{ word.value }}</h1>
+                <img v-bind:src="'/img/' + word.src"  v-bind:alt="word.value" />
+            </div>
+        </div>
+    </div> <!--modal modal_multi -->
+
   </div> <!-- Vocab -->
 </div> <!-- WordGrid -->
 </template>
